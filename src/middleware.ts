eaea47edad108c99f9a2fb3789e9fd7ca977d4cf
aull251 <h1101241025@student.untan.ac.id>
2026-03-05 22:17:36 +0700

    PRAKTIKUM 6 - afterHandle

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,39 +1,49 @@
-import { Elysia , t } from "elysia"
+import { Elysia, t } from "elysia";
 import { openapi } from "@elysiajs/openapi";
 
-
-const app = new Elysia()
-.use(openapi())
+const app = new Elysia().use(openapi());
 // Global Logger
 app.onRequest(({ request }) => {
-    console.log("📥", request.method, request.url)
-    console.log("🕒", new Date().toISOString())
-})
+    console.log("📥", request.method, request.url);
+    console.log("🕒", new Date().toISOString());
+});
 
-app.get("/", () => "APASI")
+app.get("/", () => "APASI");
 
 app.get(
     "/admin",
     () => {
     return {
-        stats: 99
-    }
+        stats: 99,
+    };
     },
     {
     headers: t.Object({
-        authorization: t.String()
+        authorization: t.String(),
     }),
     beforeHandle({ headers, set }) {
         if (headers.authorization !== "Bearer 123") {
-        set.status = 401
+        set.status = 401;
         return {
             success: false,
-            message: "Unauthorized"
+            message: "Unauthorized",
+            };
         }
-        }
-    }
-    }
-    )
+    },
+    },
+);
 
-app.listen(3000)
-console.log("Server running at http://localhost:3000")
+app.onAfterHandle(({ response }) => {
+    return {
+        success: true,
+        Message: "data tersedia",
+        data: response,
+    };
+});
+
+app.get("/product", () => {
+    return { id: 1, name: "Laptop" };
+});
+
+app.listen(3000);
+console.log("Server running at http://localhost:3000");
