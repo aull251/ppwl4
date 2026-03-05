c7e54710663237d818ae63b6a203696ca4c0b8a7
aull251 <h1101241025@student.untan.ac.id>
2026-03-05 22:27:33 +0700

    PRAKTIKUM 7 - Custom Validation Error

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,5 +45,25 @@ app.get("/product", () => {
     return { id: 1, name: "Laptop" };
 });
 
+app.onError(({ code, error, set }) => {
+    if (code === "VALIDATION") {
+    set.status = 400
+    return {
+        success: false,
+        error: "Validation Error",
+    }
+    }
+    })
+app.post(
+    "/login",
+    ({ body }) => body,
+    {
+    body: t.Object({
+        email: t.String({ format: "email" }),
+        password: t.String({ minLength: 10 })
+    })
+    }
+    )
+
 app.listen(3000);
 console.log("Server running at http://localhost:3000");
